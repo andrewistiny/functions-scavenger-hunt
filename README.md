8562a671aeb1e743c5799b6de8a2907d7e9e3026
Gina W <watanabegina26@gmail.com>
2017-08-25 23:45:33 -1000

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # functions-scavenger-hunt
+
+![Reference](https://github.com/junior-devleague/function-scavenger-hunt/blob/master/reference.png)
+
+### Objective
+
+Use your current knowledge about **JavaScript Functions**, **Arrays**, and **Objects** to complete this warmup exercise. Fork this repository and get started in your ```app.js```. The numbers that you get will correspond with the mystery objects that you must find!
+
+Make sure to link the JS file before you begin!
---
+++ app.js
@@ -0,0 +1,61 @@
+  var a = 24;
+  var b = 9;
+  var hour = 60;
+  var quarter = 4;
+  var array = [2,4,5,199,39,12,1];
+  var count = 9;
+
+  var car = {
+    wheels: 4,
+    passengers: 4,
+    mileage: 20300,
+    cost: 49928
+  }
+
+  var spaceship = {
+    wheels: 18,
+    rooms: 21,
+    passengers: 29909394895803589193049389,
+    cost: 0
+  }
+
+  var objectArray = [car, spaceship];
+
+  //PART I
+
+  //Function 1
+  //Create a function that takes in variables a and b, then divides them by 11.
+
+
+  //Function 2
+  //Create a function that will console.log a value of 18.
+
+
+  //Function 3
+  //Create a function that will console.log the last element of the array by accessing the value from the array.
+
+
+  //Function 4
+  //Create a function that can divide hours by quarters, then add 10 to that result.
+
+
+  //Function 5
+  //Create a function that will multiply the 2nd and 3rd element from the array, then subtract 5 from that result.
+
+
+  //Function 6
+  //Create a function that will subtract the number of car wheels from the number of spaceship wheels.
+
+
+  //PART II
+
+  //Function 7
+  //Create a function and using a for loop, iterate through the array. In this loop, increment the count of the variable count.
+
+
+  //Function 8
+  //Create a function and using a for loop, iterate through the objectArray. In this loop, increment the count of the passengers for the 1st element in the objectArray.
+
+
+  //Function 9
+  //Create a function that adds the 1st and 6th element of the array.
